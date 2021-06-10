3313b7c0e28111a652deec26d923c89d31fac0a5
low-boy <2206256441@qq.com>
2021-06-10 18:06:36 +0800

    创建项目

---
+++ src/main/java/com/ljf/growth/GrowthApplication.java
@@ -0,0 +1,13 @@
+package com.ljf.growth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljf/growth/GrowthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljf.growth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
